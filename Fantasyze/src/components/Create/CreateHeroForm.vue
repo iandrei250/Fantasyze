967be61820a5110bd3bfb81ebe10bbb96f7483f4
iandrei250 <prtdace@gmail.com>
2025-05-16 16:57:02 +0300

    update: fixed text

--- Fantasyze/src/components/Create/CreateHeroForm.vue
+++ Fantasyze/src/components/Create/CreateHeroForm.vue
@@ -31,7 +31,7 @@
       </div>
     </div>
     <div class="flex justify-center w-full flex-col">
-      <label for="about" class="block text-sm/6 font-medium">Description</label>
+      <label for="about" class="block text-sm/6 font-medium">Character image</label>
       <label
         for="dropzone-file"
         class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
